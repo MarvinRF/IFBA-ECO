91215a6a0c1afb7c01a7b975819e94a6e0a82c25
MarvinRF <mamavivi@gmail.com>
2025-04-04 16:33:23 -0300

    `Removed Button component from navbar`

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -45,9 +45,9 @@ export function Navbar() {
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             <ThemeToggle />
-            <Button className="bg-green-600 hover:bg-green-700">
+            {/* <Button className="bg-green-600 hover:bg-green-700">
               Participe
-            </Button>
+            </Button> */}
           </nav>
         </div>
       </div>
